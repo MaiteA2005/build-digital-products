8aa2d831d0ddf57b90fd0597f00ac00867ffda1f
Maite Aldenkamp <r0985959@student.thomasmore.be>
2025-09-22 15:59:15 +0200

    videoles 4 (code opschonen)

--- index.js
+++ index.js
@@ -6,26 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/v1/todos', function(req, res){
-    res.send("GET todos");
-});
-
-app.post('/api/v1/todos', function(req, res){
-    res.send("PORT todos");
-});
-
-app.put('/api/v1/todos/:id', function(req, res){
-    res.send("PUT todos");
-});
-
-app.delete('/api/v1/todos/:id', function(req, res){
-    res.send("DELETE todo " + req.params.id);
-});
-
 //GET api/v1/todos
+app.get('/api/v1/todos', (req, res) => {res.send("GET todos");});
+
 //POST api/v1/todos
+app.post('/api/v1/todos', (req, res) => {res.send("PORT todos");});
+
 //PUT api/v1/todos/3
+app.put('/api/v1/todos/:id', (req, res) => {res.send("PUT todos");});
+
 //DEL api/v1/todos/3
+app.delete('/api/v1/todos/:id', (req, res) => {res.send("DELETE todo " + req.params.id);});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
