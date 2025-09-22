17419e9674b674e7d88c96f37fda70245be9715a
Maite Aldenkamp <r0985959@student.thomasmore.be>
2025-09-22 14:34:40 +0200

    Videoles 3 (http en nodemon)

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const cowsay = require('cowsay');
+const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 3000;
 
-console.log(cowsay.say({
-    text: "Nodejs, let's mooooh this!"
-}));
+const server = http.createServer((req, res) => {
+    switch(req.url){
+        case "/signup":
+            res.end("This is the signup page!");
+            break;
+        default:
+            res.end("Hello world!")
+    }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
